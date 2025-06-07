62368f72ca38e20767bb919d174bb0665853586b
learningstuff1 <vinuthg2@gmail.com>
2025-06-07 18:02:03 -0500

    Finish implementation and refactor into components

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+
+const Filter = ({ filter, setFilter }) => {
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+  
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={handleFilterChange}/>
+    </div>)
+
+}
+
+const PersonForm = ({ name, setName, number, setNumber, persons, setPersons }) => {
+  const addNewPerson = (event) => {
+    event.preventDefault()
+    const newPerson = { name: name, number: number }
+    let filteredPersons = persons.filter((person) => person.name === name)
+    if (filteredPersons.length === 0) {
+      setPersons(persons.concat(newPerson))
+    } else {
+      alert(`${name} is already added to phonebook`)
+    }
+    setName('')
+    setNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value)
+  }
+
+  return (<>
+      <form onSubmit={addNewPerson}>
+        <div>
+          name: <input value={name} onChange={handleNameChange}/>
+        </div>
+        <div>number: <input value={number} onChange={handleNumberChange}/></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </>);
+}
+
+const PeopleAndNumbers = ({ persons, filter }) => {
+  return (
+    <>
+    {persons.map((person) => person.name.toLowerCase().includes(filter.toLowerCase()) ? <div key={person.name}>{person.name} {person.number}</div> : null)}
+    </>
+  )
+}
+
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  ])
+  const [newFilter, setNewFilter] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={newFilter} setFilter={setNewFilter} />
+      
+      <h2>Add a new</h2>
+      <PersonForm name={newName} setName={setNewName} number={newNumber} setNumber={setNewNumber} persons={persons} setPersons={setPersons} />
+      
+      <h2>Numbers</h2>
+      <PeopleAndNumbers persons={persons} filter={newFilter} />
+    </div>
+  )
+}
+
+export default App
