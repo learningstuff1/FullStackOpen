9b545517a52430b904b2721e12bb08f0fb96e53b
vinnysgamage <vinuthg2@gmail.com>
2025-06-20 17:05:31 -0500

    Finished implementing 2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import phonebookService from './services/phonebook.js'
+import './index.css'
 
 const Filter = ({ filter, setFilter }) => {
   const handleFilterChange = (event) => {
@@ -13,25 +14,51 @@ const Filter = ({ filter, setFilter }) => {
 
 }
 
-const PersonForm = ({ name, setName, number, setNumber, persons, setPersons }) => {
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return (
+    <div className="notif">
+      {message}
+    </div>
+  )
+}
+
+const ErrorNotif = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
+const PersonForm = ({ onAddPerson, returnPersonIfPresent, onUpdatePerson, onFailedUpdate }) => {
+  const [name, setName] = useState('')
+  const [number, setNumber] = useState('')
   const addNewPerson = (event) => {
     event.preventDefault()
     const newPerson = { name: name, number: number }
-    let filteredPersons = persons.filter((person) => person.name === name)
-    if (filteredPersons.length === 0) {
+    let personIfExists = returnPersonIfPresent(name) 
+    if (personIfExists === null) {
       phonebookService
         .create(newPerson)
         .then(returnedPerson => {
-          setPersons(persons.concat(returnedPerson))
+          onAddPerson(returnedPerson)
         })
     } else {
-      const id = filteredPersons.at(0).id 
+      const id = personIfExists.id 
       if (window.confirm(`${name} is already added to phonebook, replace the old number with a new one?`)) {
         phonebookService.update(id, newPerson)
           .then(returnedPerson => {
-            const personsRemovedUpdated = persons.filter((person) => person.name !== name)
-            setPersons(personsRemovedUpdated.concat(returnedPerson))
+            onUpdatePerson(returnedPerson)
           })
+          .catch(() => onFailedUpdate(name))
       }
     }
 
@@ -64,7 +91,7 @@ const PeopleAndNumbers = ({ persons, filter, removePerson }) => {
   const promptToRemove = (person) => {
     if (window.confirm(`Remove ${person.name}?`)) {
       phonebookService.removePerson(person.id)
-      .then(removePerson(person.id))
+      .then(removePerson(person.name))
     }
   }
 
@@ -82,8 +109,8 @@ const PeopleAndNumbers = ({ persons, filter, removePerson }) => {
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newFilter, setNewFilter] = useState('')
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [notifMessage, setNotifMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const hook = () => {
     phonebookService
@@ -95,18 +122,54 @@ const App = () => {
 
   useEffect(hook, [])
 
-  const removePerson = (id) => {
-    const filteredPersons = persons.filter(person => person.id !== id)
+  const handleAddPerson = (returnedPerson) => {
+    setPersons(persons.concat(returnedPerson))
+    setNotifMessage(`Added ${returnedPerson.name}`)
+    setTimeout(() => {
+      setNotifMessage(null)
+    }, 5000)
+  }
+
+  const returnPersonIfPresent = (name) => {
+    const filteredPersons = persons.filter((person) => person.name === name)
+    if (filteredPersons.length === 0) {
+      return null
+    }
+    return filteredPersons.at(0)
+  }
+
+  const handleUpdatePerson = (returnedPerson) => {
+    const personsRemovedUpdated = persons.filter((person) => person.name !== returnedPerson.name)
+    setPersons(personsRemovedUpdated.concat(returnedPerson))
+    setNotifMessage(`Updated number for ${returnedPerson.name}`)
+    setTimeout(() => {
+      setNotifMessage(null)
+    }, 5000)
+  }
+
+  const handleFailedUpdate = (name) => {
+    setErrorMessage(`Information of ${name} has already been removed from server`)
+    removePerson(name)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+
+  }
+
+  const removePerson = (name) => {
+    const filteredPersons = persons.filter(person => person.name !== name)
     setPersons(filteredPersons)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notifMessage}/>
+      <ErrorNotif message={errorMessage}/>
       <Filter filter={newFilter} setFilter={setNewFilter} />
       
       <h2>Add a new</h2>
-      <PersonForm name={newName} setName={setNewName} number={newNumber} setNumber={setNewNumber} persons={persons} setPersons={setPersons} />
+      <PersonForm onAddPerson={handleAddPerson} returnPersonIfPresent={returnPersonIfPresent} onUpdatePerson={handleUpdatePerson} onFailedUpdate={handleFailedUpdate}  />
       
       <h2>Numbers</h2>
       <PeopleAndNumbers persons={persons} filter={newFilter} removePerson={removePerson} />
